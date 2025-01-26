101db68b4d58900d8b7c7a62e8790b50f411f8cb
Bhavan23 <bhavansathru.it@gmail.com>
2025-01-27 03:23:23 +0530

    fix: return rejected payment status as Failed instead of Failure

--- india_banking_connector/connectors/doctype/kotak_mahindra_connector/kotak_mahindra_connector.py
+++ india_banking_connector/connectors/doctype/kotak_mahindra_connector/kotak_mahindra_connector.py
@@ -404,5 +404,5 @@ class KotakMahindraConnector(BankConnector):
 			"DC": ("Debited", "Pending"),
 			"CN": ("Cancelled", "Failed"),
 			"O": ("Draft", "Pending"),
-			"R": ("Rejected", "Failure"),
+			"R": ("Rejected", "Failed"),
 		}.get(cstr(status_code), (f"{status_code} Description Not Available", ""))
