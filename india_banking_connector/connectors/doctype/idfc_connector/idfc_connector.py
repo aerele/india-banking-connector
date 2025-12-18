ca1b7efb5f4d82774c720066962a5c1c83dd4992
Bhavan23 <bhavan@aerele.in>
2025-12-18 06:27:49 +0000

    feat(idfc): add option to generate API endpoints

--- india_banking_connector/connectors/doctype/idfc_connector/idfc_connector.py
+++ india_banking_connector/connectors/doctype/idfc_connector/idfc_connector.py
@@ -1,9 +1,27 @@
 # Copyright (c) 2025, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class IDFCConnector(Document):
-	pass
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.bank = "IDFC"
+
+	@frappe.whitelist()
+	def get_api_endpoints(self):
+		from india_banking_connector.default import IDFC_ENCRYPTED_END_POINTS
+		from india_banking_connector.install import decrypt
+
+		decrypted = decrypt(IDFC_ENCRYPTED_END_POINTS)
+		print(decrypted)
+		stagin_or_prod = "testing" if self.testing else "production"
+		endpoints = decrypted[self.bank][stagin_or_prod]["composite"]
+
+		self.api_endpoints = []
+		self.extend(
+			"api_endpoints",
+			[{"action": action, "url": url} for action, url in endpoints.items()],
+		)
