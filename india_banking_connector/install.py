74b148ceb4bd22fd578459ca00ad4bedef1f00af
Bhavan23 <bhavan@aerele.in>
2026-01-02 02:18:31 +0000

    fix(settings): update connector map

--- india_banking_connector/install.py
+++ india_banking_connector/install.py
@@ -16,6 +16,7 @@ def after_install():
 	click.secho("* Updating India Banking Connector Customisations")
 	create_bank_doctype()
 	create_default_bank()
+	create_connector_settings()
 
 
 def create_default_bank():
@@ -31,25 +32,27 @@ def create_default_bank():
 def create_connector_settings():
 	click.echo(" -> Updating Connector Settings")
 	settings_doc = frappe.get_doc("Connector Settings")
-	connector_map = [
-		{
-			"bank": bank,
-			"connector": connector,
-			"bulk_transaction": 1 if bank in BULK_TRANSACTION_ENABLED_BANK else 0,
-		}
-		for bank, connector in BANKS_CONNECTOR_MAP.items()
-		if not frappe.db.exists(
+	for bank, connector in BANKS_CONNECTOR_MAP.items():
+		if frappe.db.exists(
 			"Connector Map",
 			{
 				"bank": bank,
 				"connector": connector,
 				"bulk_transaction": 1 if bank in BULK_TRANSACTION_ENABLED_BANK else 0,
 			},
+		):
+			continue
+
+		settings_doc.append(
+			"connectors",
+			{
+				"bank": bank,
+				"connector": connector,
+				"bulk_transaction": 1 if bank in BULK_TRANSACTION_ENABLED_BANK else 0,
+			},
 		)
-	]
-	if connector_map:
-		settings_doc.extend("connectors", connector_map)
-		settings_doc.insert(ignore_links=True)
+
+	settings_doc.insert(ignore_links=True, ignore_permissions=True)
 
 
 def create_bank_doctype():
