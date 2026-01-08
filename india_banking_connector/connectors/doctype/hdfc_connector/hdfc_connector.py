c3f4ebc84a9e4f2538c422ef02ba925d04093af7
Bhavansathru <122002510+Bhavan23@users.noreply.github.com>
2026-01-08 13:21:57 +0530

    fix(api): update beneficiary name

--- india_banking_connector/connectors/doctype/hdfc_connector/hdfc_connector.py
+++ india_banking_connector/connectors/doctype/hdfc_connector/hdfc_connector.py
@@ -179,7 +179,7 @@ class HDFCConnector(BankConnector):
 				"INPUT_DEBIT_ORG_ACC_NO": conector_doc.account_number,
 				"INPUT_BUSINESS_PROD": conector_doc.business_prod,
 				"BENE_ID": "",
-				"BENE_ACC_NAME": "",
+				"BENE_ACC_NAME": payment_details.party_name,
 				"BENE_ACC_NO": payment_details.bank_account_no,
 				"BENE_TYPE": "ADHOC",
 				"BENE_BRANCH": "",
