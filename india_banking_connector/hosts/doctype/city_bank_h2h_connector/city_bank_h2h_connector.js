967831b36e5d2bca18491db6d303d7f5fb54e234
Bhavan23 <bhavan@aerele.in>
2026-01-21 07:19:55 +0000

    feat(module): add new module hosts feat(hosts): add city bank host

---
+++ india_banking_connector/hosts/doctype/city_bank_h2h_connector/city_bank_h2h_connector.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Aerele Technologies Private Limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("City Bank H2H Connector", {
+// 	refresh(frm) {
+
+// 	},
+// });
