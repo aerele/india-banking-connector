7c59f0ba08730c58c604585797055ab337d6dc28
Bhavan23 <bhavan@aerele.in>
2026-02-04 08:00:22 +0000

    feat(status): update status logic

--- india_banking_connector/hosts/doctype/base_host.py
+++ india_banking_connector/hosts/doctype/base_host.py
@@ -2,6 +2,8 @@ import frappe
 from frappe import bold
 from frappe.model.document import Document
 
+from india_banking_connector.utils import get_existing_doc, get_id
+
 
 class BaseHost(Document):
 	def is_h2h_enabled(self):
@@ -16,3 +18,14 @@ class BaseHost(Document):
 			return getattr(self, method)()
 		else:
 			frappe.throw(f"Unknown method {method} in host {self.name}")
+
+	def get_payment_status(self):
+		payment_details = frappe._dict(self.doc)
+		unique_id = get_id(payment_details.name)
+
+		existing_payment = get_existing_doc("Payment Log", unique_id)
+
+		if existing_payment:
+			return existing_payment.get_summary_details(action="get_payment_status")
+
+		return {"message": "Payment not found"}
