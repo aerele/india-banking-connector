8c5e80324ee10a327758b84f5eeff96473f301ec
Bhavan23 <bhavan@aerele.in>
2026-02-13 07:45:45 +0000

    feat(axis): add api endpoints

--- india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.py
+++ india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2026, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AxisBankConnector(Document):
-	pass
+	def __init__(self, *args, **kwargs):
+		self.bank = "Axis Bank"
+		super().__init__(*args, **kwargs)
+
+	@frappe.whitelist()
+	def get_api_endpoints(self):
+		from india_banking_connector.default import AXIS_ENCRYPTED_END_POINTS
+		from india_banking_connector.install import decrypt
+
+		decrypted = decrypt(AXIS_ENCRYPTED_END_POINTS)
+		print(decrypted)
+		self.extend(
+			"api_endpoints",
+			[{"action": action, "url": url} for action, url in decrypted.items()],
+		)
