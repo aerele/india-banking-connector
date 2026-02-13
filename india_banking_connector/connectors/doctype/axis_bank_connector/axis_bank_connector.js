760e7455ff6cbe08329a0fb7f327d7f3af58ed8e
Bhavan23 <bhavan@aerele.in>
2026-02-13 07:45:45 +0000

    feat(axis): add api endpoints
    (cherry picked from commit 8c5e80324ee10a327758b84f5eeff96473f301ec)

--- india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.js
+++ india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2026, Aerele Technologies Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Axis Bank Connector", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Axis Bank Connector", {
+	get_api_endpoints(frm) {
+		frm.call("get_api_endpoints").then(() => {
+			frm.dirty();
+		});
+	},
+});
