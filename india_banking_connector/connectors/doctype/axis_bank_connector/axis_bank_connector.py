bed87c5a82ccda5a1db0297bef6afe59955eb02d
Bhavan23 <bhavan@aerele.in>
2026-02-19 02:41:40 +0000

    feat(axis): add payment and status handlers fix(axis): correct client_id/client_secret fields type

--- india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.py
+++ india_banking_connector/connectors/doctype/axis_bank_connector/axis_bank_connector.py
@@ -2,13 +2,36 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+
+from india_banking_connector.connectors.bank_connector import BankConnector
 
 
-class AxisBankConnector(Document):
+class AxisBankConnector(BankConnector):
 	def __init__(self, *args, **kwargs):
 		self.bank = "Axis Bank"
 		super().__init__(*args, **kwargs)
+
+		self.bulk_transaction = kwargs.get("bulk_transaction")
+		self.doc = frappe._dict(kwargs.get("doc", {}))
+		self.payment_doc = frappe._dict(kwargs.get("payment_doc", {}))
+
+		self.account_config = {}
+
+	def initiate_payment(self):
+		pass
+
+	def get_payment_status(self):
+		pass
+
+	def get_bank_balance(self):
+		frappe.throw(
+			"Bank Balance API is not currently supported by the Axis Bank Connector."
+		)
+
+	def get_bank_statement(self):
+		frappe.throw(
+			"Bank statement API is not currently supported by the Axis Bank Connector."
+		)
 
 	@frappe.whitelist()
 	def get_api_endpoints(self):
