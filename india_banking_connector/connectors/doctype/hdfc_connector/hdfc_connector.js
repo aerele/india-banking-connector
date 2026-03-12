fdb22da5bb020563ba6162ca4c90235c17d3a0bf
Bhavan23 <bhavan@aerele.in>
2026-03-12 23:36:22 +0000

    refactor(hdfc-connector): migrate to generic API endpoint configuration

--- india_banking_connector/connectors/doctype/hdfc_connector/hdfc_connector.js
+++ india_banking_connector/connectors/doctype/hdfc_connector/hdfc_connector.js
@@ -1,8 +1,10 @@
-// Copyright (c) 2024, Aerele Technologies Private Limited and contributors
+// Copyright (c) 2026, Aerele Technologies Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("HDFC Connector", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("HDFC Connector", {
+	reset_endpoints(frm) {
+		frm.call("reset_endpoints").then(() => {
+			frm.dirty();
+		});
+	},
+});
