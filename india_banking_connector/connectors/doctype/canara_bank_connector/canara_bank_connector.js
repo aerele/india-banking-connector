051bdd9b67adf6b9164fb4be2c0227f9c00a0f57
Bhavan23 <bhavan@aerele.in>
2026-03-12 23:06:58 +0000

    feat(canara-bank-connector): add base URL configuration and endpoint reset support
    - extend BankConnector for Canara Bank connector
    - add base_url field for endpoint generation
    - add reset_endpoints button and client-side handler
    - include API endpoint format examples in help description

--- india_banking_connector/connectors/doctype/canara_bank_connector/canara_bank_connector.js
+++ india_banking_connector/connectors/doctype/canara_bank_connector/canara_bank_connector.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2026, Aerele Technologies Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Canara Bank Connector", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Canara Bank Connector", {
+	reset_endpoints(frm) {
+		frm.call("reset_endpoints").then(() => {
+			frm.dirty();
+		});
+	},
+});
