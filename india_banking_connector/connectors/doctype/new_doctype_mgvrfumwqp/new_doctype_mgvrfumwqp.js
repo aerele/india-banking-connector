bdf5214121ebaec9b28441ffb4b25d4738e50e81
Bhavan23 <bhavan@aerele.in>
2026-03-13 00:20:04 +0000

    fix: remove temp doc

--- india_banking_connector/connectors/doctype/new_doctype_mgvrfumwqp/new_doctype_mgvrfumwqp.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Aerele Technologies Private Limited and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("new-doctype-mgvrfumwqp", {
-// 	refresh(frm) {
-
-// 	},
-// });
