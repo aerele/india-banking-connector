ddaff787889be3b205213ed4c904581f2da83625
Bhavan23 <bhavan@aerele.in>
2026-03-13 07:18:02 +0000

    feat(connectors): add client credentials and headers support for Canara Bank connector
    - Added client_id and client_secret fields to Canara Bank Connector DocType
    - Implemented connector initialization with payment context variables
    - Added request headers generation including client credentials, certificate, timestamp, and API interaction ID
    - Normalized certificate content for header usage

--- india_banking_connector/connectors/doctype/canara_bank_connector/canara_bank_connector.py
+++ india_banking_connector/connectors/doctype/canara_bank_connector/canara_bank_connector.py
@@ -1,8 +1,43 @@
 # Copyright (c) 2026, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
+import frappe
+from frappe.utils import cstr, get_datetime
+
 from india_banking_connector.connectors.bank_connector import BankConnector
 
 
 class CanaraBankConnector(BankConnector):
 	bank = "Canara Bank"
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+		self.bulk_transaction = kwargs.get("bulk_transaction")
+		self.doc = frappe._dict(kwargs.get("doc", {}))
+		self.payment_doc = frappe._dict(kwargs.get("payment_doc", {}))
+
+		self.account_config = {}
+
+	@property
+	def urls(self):
+		return super().urls
+
+	@property
+	def headers(self):
+		cert = self.get_file_content(self.cert)
+		cert_key = (
+			cert.lstrip("-----BEGIN CERTIFICATE-----")
+			.rstrip("-----END CERTIFICATE-----")
+			.strip()
+		)  # normalise
+
+		return {
+			"x-client-id": self.client_id,
+			"x-client-secret": self.get_password("client_secret"),
+			"x-client-certificate": cert_key,
+			"x-api-interaction-id": "1",
+			"x-timestamp": cstr(int(get_datetime().timestamp() * 1000)),
+			"x-signature": "",
+			"Content-Type": "application/json",
+		}
