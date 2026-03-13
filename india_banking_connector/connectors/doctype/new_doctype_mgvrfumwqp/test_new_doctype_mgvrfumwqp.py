03c090559fac122f83ef0ec829159d1bdda6f08e
Bhavan23 <bhavan@aerele.in>
2026-03-13 00:20:04 +0000

    fix: remove temp doc

--- india_banking_connector/connectors/doctype/new_doctype_mgvrfumwqp/test_new_doctype_mgvrfumwqp.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Aerele Technologies Private Limited and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testnewdoctypemgvrfumwqp(FrappeTestCase):
-	pass
