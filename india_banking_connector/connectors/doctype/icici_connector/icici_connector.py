828a807e26e9576d174ae42e91cc6e51de81ed13
Sakthi Kumar Pugalenthy <sakthi123msd@gmail.com>
2026-07-20 15:05:48 +0530

    Revert "fix(icici): map PENDING FOR PROCESSING / PROCESSING to Pending row status"
    This reverts commit bfc2dae796386a7f1dbae4d5f00fa81974e21817.

--- india_banking_connector/connectors/doctype/icici_connector/icici_connector.py
+++ india_banking_connector/connectors/doctype/icici_connector/icici_connector.py
@@ -717,12 +717,7 @@ class ICICIConnector(BankConnector):
 						"message": data.MESSAGE or "Payment Completed",
 					}
 				}
-			elif data.STATUS in [
-				"PENDING",
-				"PENDING FOR PROCESSING",
-				"PROCESSING",
-				"PENDING FOR APPROVAL",
-			]:
+			elif data.STATUS in ["PENDING", "PENDING FOR APPROVAL"]:
 				res_dict.payment_status = "PROCESSED"
 				res_dict.summary_details = {
 					self.payment_doc.name: {
